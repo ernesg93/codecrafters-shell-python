897da24b16397aeaee4587d679433de97eadb099
R2D2 <ernesg93@gmail.com>
2025-10-21 23:27:22 -0400

    [any message]

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ def main():
     sys.stdout.write("$ ")
 
     # Wait for user input
-    input()
+    command = input()
 
     # Prints the "<command>: command not found" message
     print(f"{command}: command not found")
