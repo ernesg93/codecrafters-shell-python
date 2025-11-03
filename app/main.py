503d5b0016d70b2bcf3f375999cc8eb6acc41375
R2D2 <ernesg93@gmail.com>
2025-11-03 15:35:18 -0500

    ejercicio

--- app/main.py
+++ app/main.py
@@ -3,15 +3,17 @@ import sys
 
 def main():
     while True:
-
         sys.stdout.write("$ ")
-
         # Wait for user input
-        command = input()
+        command = input().strip()
+        if not command:
+            continue
 
-        # Prints the "<command>: command not found" message
-        print(f"{command}: command not found")
-
+        if command.startswith("exit"):
+            sys.exit(0)
+        else:
+            # Prints the "<command>: command not found" message
+            print(f"{command.split()[0]}: command not found")
 
 if __name__ == "__main__":
     main()
